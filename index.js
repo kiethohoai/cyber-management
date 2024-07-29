e2fea566128ffedd1bcc2f3d13ffbb9ba3252a9d
kiethohoai <kietho.060193@gmail.com>
2024-07-29 14:32:01 +0700

    03 Model & Sync

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-const { Sequelize } = require('sequelize');
+// todo Define
+const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize('tm', 'root', '123456', {
   host: 'localhost',
   dialect: 'mysql',
 });
 
+// todo Check Connect
 const checkConnection = async () => {
   try {
     await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
+    console.log('Connection Database successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error.message);
   }
 };
-
 checkConnection();
+
+// todo Create Modal (Table in MySQL)
+const Task = sequelize.define('Task', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+// todo Model synchronization
+const syncModal = async () => {
+  // await Task.sync({ alter: true });
+  await Task.sync({ force: true });
+  console.log('The table for the Task model was just (re)created!');
+};
+syncModal();
